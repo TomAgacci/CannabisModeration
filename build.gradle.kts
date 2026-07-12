bdd6923e32b2be88f5acfcb0682d3adf11df2315
TomAgacci <dementedmax@gmail.com>
2026-07-11 18:14:18 -0700

    Create build.gradle.kts

---
+++ build.gradle.kts
@@ -0,0 +1,9 @@
+plugins {
+    kotlin("multiplatform") version "2.0.0" apply false
+    id("org.jetbrains.compose") version "1.6.10" apply false
+}
+
+repositories {
+    google()
+    mavenCentral()
+}
